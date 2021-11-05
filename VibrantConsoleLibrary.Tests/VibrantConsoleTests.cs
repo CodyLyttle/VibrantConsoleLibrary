465c6a1b0b216042942da18515a54a35cbe99adc
Cody Lyttle <CodyLyttle123@gmail.com>
2021-11-05 15:53:34 +1300

    Initial commit

---
+++ VibrantConsoleLibrary.Tests/VibrantConsoleTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Xunit;
+// ReSharper disable Xunit.XunitTestWithConsoleOutput
+
+namespace VibrantConsoleLibrary.Tests
+{
+    public class VibrantConsoleTests
+    {
+        [Fact]
+        public void WriteLine_OnString_WritesStringToConsole()
+        {
+            StringWriter writer = new();
+            Console.SetOut(writer);
+            const string input = "Test";
+            VibrantConsole.WriteLine(input);
+            Console.SetOut(Console.Out);
+
+            const string expected = input + "\r\n";
+            string actual = writer.ToString();
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ VibrantConsoleLibrary/VibrantConsole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("VibrantConsoleLibrary.Tests")]
+namespace VibrantConsoleLibrary
+{
+    
+    public static class VibrantConsole
+    {
+        public static void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }        
+    }
+}
